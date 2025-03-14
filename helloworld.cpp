c307634bbdef5874f1305b68c7836209efa59bcf
cheng.zihan <1147562979@qq.com>
2025-03-14 21:31:39 +0800

    add vector

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,10 +7,11 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
- 
+    auto a = msg.begin();
     for (const string& word : msg)
     {
         cout << word << " ";
     }
     cout << endl;
+    cout << msg[msg.size() -1 ] << endl;
 }
